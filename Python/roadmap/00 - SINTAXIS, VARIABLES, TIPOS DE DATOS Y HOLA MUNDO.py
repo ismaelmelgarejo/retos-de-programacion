df06942e24e9943192b4d201b4049b86f1ea3d08
Ismael Melgarejo <ismaelmelgarejo@gmail.com>
2024-09-25 16:03:17 -0400

    Principal

---
+++ Python/roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO.py
@@ -0,0 +1,31 @@
+import sys
+import tty
+import termios
+print(" ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?")
+print(" - Recuerda que todas las instrucciones de participación están en el")
+print("   repositorio de GitHub.")
+print("*")
+print(" Lo primero... ¿Ya has elegido un lenguaje?")
+print(" - No todos son iguales, pero sus fundamentos suelen ser comunes.")
+print(" - Este primer reto te servirá para familiarizarte con la forma de participar")
+print("   enviando tus propias soluciones.")
+print("*")
+print(" EJERCICIO:")
+print(" 1 - Crea un comentario en el código y coloca la URL del sitio web oficial del")
+print("   lenguaje de programación que has seleccionado.")
+print(" 2 - Representa las diferentes sintaxis que existen de crear comentarios")
+print("   en el lenguaje (en una línea, varias...).")
+print(" 3 - Crea una variable (y una constante si el lenguaje lo soporta).")
+print(" 4 - Crea variables representando todos los tipos de datos primitivos")
+print("   del lenguaje (cadenas de texto, enteros, booleanos...).")
+print(' 5 - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"')
+print("*")
+print(" ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y")
+print(" debemos comenzar por el principio.")
+
+#https://python.org
+
+print('Comentario de una Linea (#)')
+print('Comentario de un bloque (""") ')
+
+
